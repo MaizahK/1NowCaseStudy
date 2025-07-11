0c0a813d6905cba2c74900f446007728c46dcd92
Maizah Kaleem <maizahkaleem1@gmail.com>
2025-07-11 14:11:53 +0500

    allow swagger to access endpoints without user constraints and use generics APIView to limit endpoints

--- bookings_app/urls.py
+++ bookings_app/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from .views import BookingViewSet
 
-router = DefaultRouter()
-router.register(r'', BookingViewSet, basename='booking')
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', BookingViewSet.as_view(), name='booking-list-create'),
 ]
--- bookings_app/views.py
+++ bookings_app/views.py
@@ -1,17 +1,20 @@
-from rest_framework import viewsets, permissions
-from rest_framework import filters
+from rest_framework import generics, permissions, filters
 from .models import Booking
 from .serializers import BookingSerializer
 from .services import stripe_deposit
 
-class BookingViewSet(viewsets.ModelViewSet):
+# generics APIView inherited to limit endpoints to GET and POST
+class BookingViewSet(generics.ListCreateAPIView):
     serializer_class = BookingSerializer
     permission_classes = [permissions.IsAuthenticated]
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['start_date', 'end_date']
 
     def get_queryset(self):
-        qs = Booking.objects.filter(user=self.request.user)
+        if getattr(self, 'swagger_fake_view', False):
+            return Booking.objects.none()
+        qs = Booking.objects.filter(user=self.request.user).order_by('-id')
+
         from_date = self.request.query_params.get('from')
         if from_date:
             qs = qs.filter(start_date__gte=from_date)
