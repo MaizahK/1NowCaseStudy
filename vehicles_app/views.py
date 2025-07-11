a8f4bbfc74bd76281902ce2b3581f0221f5f88f0
Maizah Kaleem <maizahkaleem1@gmail.com>
2025-07-11 14:13:05 +0500

    mixins and class variable to limit request methods and for representation in swagger docs

--- vehicles_app/views.py
+++ vehicles_app/views.py
@@ -1,13 +1,21 @@
-from rest_framework import viewsets, permissions
+from rest_framework import mixins, viewsets, permissions
 from .models import Vehicle
 from .serializers import VehicleSerializer
 
-class VehicleViewSet(viewsets.ModelViewSet):
+# specific mixins inherited to disallow GET /vehicles/{id} endpoint 
+class VehicleViewSet(mixins.ListModelMixin,
+                     mixins.CreateModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     viewsets.GenericViewSet):
     serializer_class = VehicleSerializer
     permission_classes = [permissions.IsAuthenticated]
+    http_method_names = ['get', 'post', 'put', 'delete']    # disallow partial update (PATCH /vehicles/{id})
 
     def get_queryset(self):
-        return Vehicle.objects.filter(owner=self.request.user)
+        if getattr(self, 'swagger_fake_view', False):
+            return Vehicle.objects.none()
+        return Vehicle.objects.filter(owner=self.request.user).order_by('-id')
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
