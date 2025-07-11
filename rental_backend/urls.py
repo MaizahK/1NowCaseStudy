8a3ab79428d104d31e51c7b94ce3485ddc10ed72
Maizah Kaleem <maizahkaleem1@gmail.com>
2025-07-11 14:12:14 +0500

    token login endpoints moved to user app

--- rental_backend/urls.py
+++ rental_backend/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -35,10 +34,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('api/users/', include('users_app.urls')),
-    path('api/vehicles/', include('vehicles_app.urls')),
-    path('api/bookings/', include('bookings_app.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('users_app.urls')),
+    path('vehicles/', include('vehicles_app.urls')),
+    path('bookings/', include('bookings_app.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
